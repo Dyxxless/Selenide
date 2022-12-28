8176472a50d2e6f18aca800736b8f73de5a0244e
Dyxxless <79612696784@yandex.ru>
2022-12-28 23:21:58 +0300

    Add .appveyor.yml and finish test

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,21 +1,34 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Visible;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryTest {
+    String deliveryDate() {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        //находим самую близкую дату доставки. 3 дня от сегодняшнего
+    }
+
     @Test
     void test() {
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
+        String date = deliveryDate();
         $("[placeholder=\"Город\"]").setValue("Пермь");
         $("[name=\"name\"]").setValue("Роман Романов");
         $("[name=\"phone\"]").setValue("+79991234567");
         $("[class=\"checkbox__box\"]").click();
         $("[class=\"button__text\"]").click();
-
-
+        $("[class=\"notification__title\"]").should(visible, Duration.ofSeconds(15));
+        $("[class=\"notification__content\"]").shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
     }
 }
